119983b401291fe6065564b5893f34a3b0619bb3
Lucas de Souza Santos <lucas.santoss210@gmail.com>
2024-10-15 21:21:55 -0300

    first program

--- SchellScript-Folder/helloworld.sh
+++ SchellScript-Folder/helloworld.sh
@@ -1,6 +1,10 @@
 #!/bin/sh 
 
-echo "hello world"
-echo "What is your name ?"
-read PERSON
-echo "Hello,  $PERSON"
+# echo "hello world"
+# echo "What is your name ?"
+# read PERSON
+# echo "Hello,  $PERSON"
+
+date
+read data
+echo "A data de hoje e $data"
