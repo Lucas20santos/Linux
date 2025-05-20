6f3e2c1a183d5e4a3ecc4e9ee1f522372eb4cf6e
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-05-20 00:43:35 +0000

    lendo os arquivos.

--- Santander - Linux para Iniciantes/projects/criando_usuario.sh
+++ Santander - Linux para Iniciantes/projects/criando_usuario.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
-echo "Lendo lista ADM.txt"
+echo "Começando a leitura dos arquivos..."
 
 arquivo_adm="usuarios/adm.txt"
+arquivo_sec="usuarios/sec.txt"
+arquivo_ven="usuarios/ven.txt"
 
-# Verificando se arquivo existe
+# Verificando se arquivo existe e fazendo a leitura dos arquivos
+# leitura do arquivo adm.txt
 if [ ! -f "$arquivo_adm" ]; then
-echo "Erro: Arquivo '$arquivo_adm' não encontrado."
-exit 1
+    echo "Erro: Arquivo '$arquivo_adm' não encontrado."
+    exit 1
+else
+    while IFS= read -r usuario || [[ -n "$usuario" ]]; do
+
+        echo "Processando usuário: $usuario"
+
+        done < "$arquivo_adm"
+
+        echo "Leitura do arquivo: '$arquivo_adm' concluída"
 fi
 
-echo "Lendo usuários do arquivo '$arquivo_adm'"
+# leitura do arquivo sec.txt
+if [ ! -f "$arquivo_sec" ]; then
+    echo "Erro: Arquivo '$arquivo_sec' não encontrado."
+    exit 1
+else
+    while IFS= read -r usuario || [[ -n "$usuario" ]]; do
 
-while IFS= read -r usuario || [[ -n "$usuario" ]]; do
+        echo "Processando usuário: $usuario"
 
-echo "Processando usuário: $usuario"
+        done < "$arquivo_sec"
 
-done < "$arquivo_adm"
+        echo "Leitura do arquivo: '$arquivo_sec' concluída"
+fi
 
-echo "Leitura do arquivo concluída"
+# leitura do arquivo ven.txt
+if [ ! -f "$arquivo_ven" ]; then
+    echo "Erro: Arquivo '$arquivo_ven' não encontrado."
+    exit 1
+else
+    while IFS= read -r usuario || [[ -n "$usuario" ]]; do
+
+        echo "Processando usuário: $usuario"
+
+        done < "$arquivo_ven"
+
+        echo "Leitura do arquivo: '$arquivo_ven' concluída"
+fi
