e8c4effc33d1cff60e4d53e638798d959b2cd36f
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-05-20 23:19:03 +0000

    comentando parte do codigo para tirar alguns alertas

--- Santander - Linux para Iniciantes/projects/script/menu.sh
+++ Santander - Linux para Iniciantes/projects/script/menu.sh
@@ -3,9 +3,9 @@
 # --- Cores AANSI para melhar a aparência (Opcional) ---
 VERMELHO='\033[0;31m'
 VERDE='\033[0;32m'
-AMARELO='\033[0;33m'
+#AMARELO='\033[0;33m'
 AZUL='\033[0;34m'
-ROXO='\033[0;35m'
+#ROXO='\033[0;35m'
 CIANO='\033[0;36m'
 BRANCO='\033[0;37m'
 RESET='\033[0m'
